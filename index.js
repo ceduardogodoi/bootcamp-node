74e117eb7b2379c200a16cc439e7213901f3dc96
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-10-16 00:12:34 -0300

    Utilizando o Insomnia, o Nodemon e criando o CRUD

--- index.js
+++ index.js
@@ -2,14 +2,49 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "name": "Diego", "email": "diego@rocketseat.com.br" }
 
-server.get('/users/:id', (req, res) => {
-  const { id } = req.params;
+// CRUD - Create, Read, Update e Delete
 
-  return res.json({ message: `Buscando o usuário: ${id}` });
+const users = ['Diego', 'Robson', 'Victor'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
