fc1e70610130adbebf05da5ccaff8c949050b653
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-10-15 23:13:26 -0300

    Diferenciando entre o query params, route params e o request body

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const server = express();
 
-// localhost:3000/teste
-server.get('/teste', (req, res) => {
-  return res.json({ message: 'Hello World' });
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "Diego", "email": "diego@rocketseat.com.br" }
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  return res.json({ message: `Buscando o usuário: ${id}` });
 });
 
 server.listen(3000);
